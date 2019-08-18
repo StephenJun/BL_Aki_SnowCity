70d7cf965187e6bb45f7d1a62b9e5d1729ebe3f6
StephenJun <StephenJun@users.github.com>
2019-08-18 20:58:35 +0800

    操作优化

---
+++ Assets/Scripts/Controller/ThirdPersonInteraction.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonInteraction : MonoBehaviour {
+
+    public float maxSearchDis = 5.0f;
+
+    private InteractableObj interactableObj;
+
+    private Camera m_camera;
+
+    private void Start()
+    {
+        m_camera = Camera.main;
+    }
+
+    public void SearchInteractableObj()
+    {
+        Ray ray = m_camera.ScreenPointToRay(new Vector2(Screen.width, Screen.height)/2);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, maxSearchDis))
+        {
+            InteractableObj obj = hit.collider.GetComponentInChildren<InteractableObj>();
+            if(obj != null)
+            {
+                interactableObj = obj;
+            }
+        }
+    }
+
+}
